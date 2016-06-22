f632cc6f87084f0e6c7bd9f11ed58033e227a196
R.H. van Heukelum <r.h.vanheukelum@student.tudelft.nl>
2016-06-22 15:10:40 +0200

    Added Proof of Concept for AaronAPIAdapter

--- src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
+++ src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
@@ -1,7 +1,15 @@
 package org.sonarsource.plugins.example.DatabaseAdapter;
 
+import org.json.JSONObject;
+
 /**
  * Created by Mathias on 2016-06-21.
  */
 public class AaronAPIAdapter {
+    private final static String IP = "http://146.185.128.124/api/";
+    private final static String FORMAT = "/?format=json";
+
+    public JSONObject getUsers() {
+        return JSONReader.getJSON(IP + "users" + FORMAT);
+    }
 }
