ea62a622288de425ea9d969f216fb5a0d2f64608
R.H. van Heukelum <r.h.vanheukelum@student.tudelft.nl>
2016-06-23 11:47:49 +0200

    String to arraylist in getUsernames

--- src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
+++ src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
@@ -1,6 +1,9 @@
 package org.sonarsource.plugins.example.DatabaseAdapter;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Mathias on 2016-06-21.
@@ -9,7 +12,14 @@ public class AaronAPIAdapter {
     private final static String IP = "http://146.185.128.124/api/";
     private final static String FORMAT = "/?format=json";
 
-    public JSONObject getUsers() {
-        return JSONReader.getJSON(IP + "users" + FORMAT);
+    public ArrayList<String> getUsernames() {
+        JSONObject json = JSONReader.getJSON(IP + "users" + FORMAT);
+        JSONArray array = json.getJSONArray("results");
+        ArrayList<String> names = new ArrayList<>();
+        for (int user = 0; user < array.length(); user++) {
+            names.add(((JSONObject) array.get(user)).get("username").toString());
+        }
+        return names;
     }
+
 }
