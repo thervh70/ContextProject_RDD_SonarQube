545fa4a17544bb82ffbebc25f515bfabdcc1ebf3
R.H. van Heukelum <r.h.vanheukelum@student.tudelft.nl>
2016-06-23 12:53:54 +0200

    Added new Adapter method to AaronAPI

--- src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
+++ src/main/java/org/sonarsource/plugins/example/DatabaseAdapter/AaronAPIAdapter.java
@@ -2,6 +2,8 @@ package org.sonarsource.plugins.example.DatabaseAdapter;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.sonarsource.plugins.example.entities.Repository;
+import org.sonarsource.plugins.example.entities.User;
 
 import java.util.ArrayList;
 
@@ -12,14 +14,31 @@ public class AaronAPIAdapter {
     private final static String IP = "http://146.185.128.124/api/";
     private final static String FORMAT = "/?format=json";
 
-    public ArrayList<String> getUsernames() {
+    public ArrayList<User> getUsers() {
         JSONObject json = JSONReader.getJSON(IP + "users" + FORMAT);
         JSONArray array = json.getJSONArray("results");
-        ArrayList<String> names = new ArrayList<>();
-        for (int user = 0; user < array.length(); user++) {
-            names.add(((JSONObject) array.get(user)).get("username").toString());
+        ArrayList<User> users = new ArrayList<>();
+        for (int i = 0; i < array.length(); i++) {
+            String username = ((JSONObject) array.get(i)).get("username").toString();
+            users.add(new User(username));
         }
-        return names;
+        getRepositories(users);
+        return users;
     }
 
+    public void getRepositories(ArrayList<User> userList) {
+        JSONObject json = JSONReader.getJSON(IP + "repositories" + FORMAT);
+        JSONArray array = json.getJSONArray("results");
+        for (User user : userList) {
+            for (int i = 0; i < array.length(); i++) {
+                String platform = ((JSONObject) array.get(i)).get("platform").toString();
+                String owner = ((JSONObject) array.get(i)).get("owner").toString();
+                if (platform.equals("GitHub") && owner.equals(user.getName())) {
+                    String reponame = ((JSONObject) array.get(i)).get("name").toString();
+                    user.addRepository(new Repository(reponame, user, ""));
+                }
+            }
+
+        }
+    }
 }
